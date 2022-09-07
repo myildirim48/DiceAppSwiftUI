d5ee374648e2dd04081567ba5592d3b805820981
Muhammed YILDIRIM <82388073+myildirim48@users.noreply.github.com>
2022-09-08 01:05:49 +0300

    Add files via upload

---
+++ DiceAppUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiceAppUI
+//
+//  Created by YILDIRIM on 8.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!ddasdasd")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceAppUI/DiceAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceAppUIApp.swift
+//  DiceAppUI
+//
+//  Created by YILDIRIM on 8.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct DiceAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
